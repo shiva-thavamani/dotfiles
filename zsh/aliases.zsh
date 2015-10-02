0b1b796162d4f30268afab4047872151cda51477
Chris DiMartino <vwchris@gmail.com>
2015-10-02 15:23:40 -0400

    zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,21 @@
 alias reload!='. ~/.zshrc'
+
+
+BASE="$HOME/Documents"
+SOURCE="$BASE/Source"
+hash -d gitrepos="$BASE/Source/git"
+hash -d ewn="/Users/chris/Source/EWN/newspresso/"
+hash -d dbc="/Users/chris/Source/DBC/"
+hash -d cic="/Users/chris/Source/DBC/Cohorts/2015/NYC/cicadas/"
+hash -d in="/Users/chris/Inbox/"
+
+alias unpushed='git log --branches --not --remotes'
+alias unpushed_summary='git log --branches --not --remotes --simplify-by-decoration --decorate --oneline'
+
+alias less='less -R'
+alias top='top -s1 -o cpu -R -F'
+alias gf='git-flow'
+
+alias rakedb='be rake db:drop; be rake db:create; be rake db:migrate; be rake db:seed'
+alias be='bundle exec'
+
---
+++ zsh/path.zsh
@@ -0,0 +1,12 @@
+#
+# Customize to your needs...
+export PATH=${HOME}/bin:${HOME}/.bin:${HOME}/gocode/bin:/usr/local/bin:/usr/local/sbin:/bin:/sbin::/opt/cloudmaster/tools:/opt/ec2-api-tools/bin:/opt/iam_cli/bin:/Documents/Infrastructure/src/cloudmaster/tools:$PATH
+
+### Added by the Heroku Toolbelt
+export PATH="/usr/local/heroku/bin:$PATH"
+
+### For Homebrew ###
+export PATH="/usr/local/Cellar/bin":$PATH;
+
+### For US!! ###
+export PATH=$PATH:~/.dotfiles/bin
